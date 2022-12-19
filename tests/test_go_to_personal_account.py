ea9b109a960b1bd7fd537058e34e82e676316226
FrancevN <francevnik@icloud.com>
2022-12-19 17:51:30 +0400

    V 2.1

--- tests/test_go_to_personal_account.py
+++ tests/test_go_to_personal_account.py
@@ -12,4 +12,4 @@ def test_go_to_personal_account_open_personal_account(driver):
     driver.find_elements(*Locators.header_button)[2].click()
     WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.info_in_personal_account))
     assert driver.find_element(*Locators.info_in_personal_account).text == 'В этом разделе вы можете изменить свои ' \
-                                                                           'персональные данные '
+                                                                           'персональные данные'
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,7 @@
 from uuid import uuid4
 from locators import Locators
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def test_registration_new_user_add_user(driver):
@@ -8,6 +10,7 @@ def test_registration_new_user_add_user(driver):
     driver.find_elements(*Locators.login)[1].send_keys(str(uuid4()) + '@gmail.com')
     driver.find_element(*Locators.password).send_keys(str(uuid4()))
     driver.find_element(*Locators.registration_button).click()
+    WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.login_forgot_password))
     assert driver.current_url == 'https://stellarburgers.nomoreparties.site/login'
 
 
